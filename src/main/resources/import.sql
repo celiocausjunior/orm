c2a6d6f5cc84e6b4bed637d8d6071da4be7170a5
Celio Caus Junior <celiocausjunior@Mac-mini-de-Celio.local>
2023-07-28 20:20:04 -0300

    Orm finished

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,9 +3,6 @@ INSERT INTO tb_participante (id, nome, email) VALUES (2, 'Tiago Faria', 'tiago@g
 INSERT INTO tb_participante (id, nome, email) VALUES (3, 'Maria do Rosario', 'maria@gmail.com');
 INSERT INTO tb_participante (id, nome, email) VALUES (4, 'Teresa Silva', 'teresa@gmail.com');
 
-INSERT INTO tb_bloco (id, inicio, fim) VALUES (1, '2017-09-25T08:00:00.000Z', '2017-09-25T11:00:00.000Z');
-INSERT INTO tb_bloco (id, inicio, fim) VALUES (2, '2017-09-25T14:00:00.000Z', '2017-09-25T18:00:00.000Z');
-
 INSERT INTO tb_categoria(id, descricao) VALUES (1, 'Curso');
 INSERT INTO tb_categoria(id, descricao) VALUES (2, 'Oficina');
 
@@ -18,5 +15,10 @@ INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (1,
 INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (2, 1);
 INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (2, 3);
 INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (2, 4);
+
+INSERT INTO tb_bloco (id, inicio, fim, atividade_id) VALUES (1, '2017-09-25T08:00:00.000Z', '2017-09-25T11:00:00.000Z', 1);
+INSERT INTO tb_bloco (id, inicio, fim, atividade_id) VALUES (2, '2017-09-25T14:00:00.000Z', '2017-09-25T18:00:00.000Z', 2);
+INSERT INTO tb_bloco (id, inicio, fim, atividade_id) VALUES (3, '2017-09-26T08:00:00.000Z', '2017-09-26T11:00:00.000Z', 2);
+
 
 
