67f40d0dbc061aeb7c73cec3a569020ab5c021d3
Celio Caus Junior <celiocausjunior@Mac-mini-de-Celio.local>
2023-07-28 19:21:02 -0300

    first commit

---
+++ src/test/java/celiocausjunior/desafioORM/DesafioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package celiocausjunior.desafioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
